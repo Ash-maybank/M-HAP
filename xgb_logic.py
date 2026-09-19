0778caef9cc52d1aa30944034b353f3b1fe048ed
Ash-maybank <121545161+Ash-maybank@users.noreply.github.com>
2026-09-19 22:52:38 +0800

    Update xgb_logic.py

--- xgb_logic.py
+++ xgb_logic.py
@@ -16,7 +16,7 @@ def train_and_evaluate(data):  # Make sure the parameter is named 'data'
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    model = XGBRegressor(objective='reg:squarederror', n_estimators=100, seed=100)
+    model = XGBRegressor(objective='reg:gamma', n_estimators=100, seed=100)
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
